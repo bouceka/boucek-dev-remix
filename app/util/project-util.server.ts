c8f732b8046b98a72234c386028ce39be2124164
bouceka <adamm01b@gmail.com>
2023-04-16 20:58:26 -0700

    debug: Debug projects path

--- app/util/project-util.server.ts
+++ app/util/project-util.server.ts
@@ -15,24 +15,22 @@ interface MarkdownData {
 }
 
 const postsDirectory =
-  process.env.NODE_ENV === 'production' ? __dirname + '/posts' : path.join(process.cwd(), 'public/posts');
+  process.env.NODE_ENV === 'production' ? '/posts' : path.join(process.cwd(), 'public/posts');
 
-console.log(postsDirectory);
+	console.log(postsDirectory)
 export function getPostData(postIdentifier: string) {
-  try {
-    const postSlug = postIdentifier.replace(/\.md$/, ''); // removes the file extension
-    const filePath = path.join(postsDirectory, `${postSlug}.md`);
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const { data, content } = matter(fileContent);
-    const postData = {
-      slug: postSlug,
-      ...(data as MarkdownData),
-      content,
-    };
-    return postData;
-  } catch (e) {
-    console.log(e);
-  }
+  const postSlug = postIdentifier.replace(/\.md$/, ''); // removes the file extension
+  const filePath = path.join(postsDirectory, `${postSlug}.md`);
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const { data, content } = matter(fileContent);
+
+  const postData = {
+    slug: postSlug,
+    ...(data as MarkdownData),
+    content,
+  };
+
+  return postData;
 }
 
 export function getPostsFiles() {
