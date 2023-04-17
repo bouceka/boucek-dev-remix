4673f6060f3f0e7478180ca22f60f8d68f65a093
bouceka <adamm01b@gmail.com>
2023-04-16 20:20:57 -0700

    debug: Debug homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,11 +8,11 @@ export const meta: V2_MetaFunction = () => {
   return [{ title: 'New Remix App' }];
 };
 
-// export const loader = async () => {
-//   const posts = getAllPosts();
+export const loader = async () => {
+  const posts = getAllPosts();
 
-//   return json({ posts });
-// };
+  return json({ posts });
+};
 
 export default function Index() {
 	// const { posts } = useLoaderData<typeof loader>();
