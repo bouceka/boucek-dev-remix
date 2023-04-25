abc9c2154ab914c13b4473a7168639f79e0b8448
Adam Boucek <adamm01b@gmail.com>
2023-04-25 11:55:17 -0700

    feat: Add admin routes

---
+++ app/routes/admin/add-project.tsx
@@ -0,0 +1,19 @@
+// @flow
+import * as React from 'react';
+type Props = {};
+const AddProject = (props: Props) => {
+  return (
+    <main className='page'>
+      <div className='row'>
+        <h1>Add project</h1>
+        <form action=''>
+          <div className="form-group">
+            
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default AddProject;
---
+++ app/routes/admin/admin.tsx
@@ -0,0 +1,12 @@
+// @flow
+import * as React from 'react';
+type Props = {};
+const Admin = (props: Props) => {
+  return (
+    <main>
+      <h1>Admin</h1>
+    </main>
+  );
+};
+
+export default Admin;
