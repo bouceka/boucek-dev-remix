db830553cc270f0f2882d11190280af4f5c88472
Adam Boucek <adamm01b@gmail.com>
2023-04-27 17:37:07 -0700

    feat: Add create project form page

--- app/routes/admin.add-project.tsx
+++ app/routes/admin.add-project.tsx
@@ -1,79 +1,111 @@
 // @flow
+import { withZod } from '@remix-validated-form/with-zod';
 import * as React from 'react';
-type Props = {};
-const AddProject = (props: Props) => {
+import { redirect } from 'remix-typedjson';
+import { ValidatedForm, validationError } from 'remix-validated-form';
+import { addProject } from '~/data/projects.server';
+import type { ActionArgs } from '@remix-run/node';
+import { z } from 'zod';
+
+const validator = withZod(
+  z.object({
+    title: z.string().min(1, { message: "Title can't be empty" }),
+    slug: z.string().min(1, { message: "Slug can't be empty" }),
+    excerpt: z.string().min(1, { message: "Excerpt can't be empty" }),
+    isFeatured: z.string().min(1, { message: "Is Featured can't be empty" }),
+    githubURL: z.string().min(1, { message: "GitHub URL can't be empty" }),
+    websiteURL: z.string().min(1, { message: "Website URL can't be empty" }),
+    coverImage: z.string().min(1, { message: "Cover Image can't be empty" }),
+    markdown: z.string().min(1, { message: "Markdown can't be empty" }),
+  })
+);
+const AddProject = () => {
+  const subjectFormValidator = validator;
   return (
     <main>
       <div className='row'>
         <h3>Add project</h3>
-        <form action=''>
+        <ValidatedForm validator={subjectFormValidator} method='post'>
           <div className='form-container'>
             <div className='form-item'>
-              <label htmlFor='email'>Title *</label>
+              <label htmlFor='title'>Title *</label>
               <input
                 placeholder='...'
                 type='text'
                 // {...register('email')}
-                id='email'
+                id='title'
+                name='title'
+                required
               />
             </div>
             <div className='form-item'>
-              <label htmlFor='email'>Slug *</label>
+              <label htmlFor='slug'>Slug *</label>
               <input
                 placeholder='...'
                 type='text'
                 // {...register('email')}
-                id='email'
+                id='slug'
+                name='slug'
               />
             </div>
             <div className='form-item'>
-              <label htmlFor='email'>Excerpt *</label>
+              <label htmlFor='excerpt'>Excerpt *</label>
               <input
                 placeholder='...'
                 type='text'
                 // {...register('email')}
-                id='email'
+                id='excerpt'
+                name='excerpt'
+                required
               />
             </div>
             <div className='form-item'>
-              <label htmlFor='email'>Is Featured *</label>
+              <label htmlFor='isFeatured'>Is Featured *</label>
               <input
                 placeholder='...'
                 type='text'
                 // {...register('email')}
-                id='email'
+                id='isFeatured'
+                name='isFeatured'
+                required
               />
             </div>
             <div className='form-item'>
-              <label htmlFor='email'>Github Link *</label>
+              <label htmlFor='githubURL'>Github Link *</label>
               <input
                 placeholder='...'
                 type='text'
                 // {...register('email')}
-                id='email'
+                id='githubURL'
+                name='githubURL'
+                required
               />
             </div>
             <div className='form-item'>
-              <label htmlFor='email'>Website Link *</label>
+              <label htmlFor='websiteURL'>Website Link *</label>
               <input
                 placeholder='...'
                 type='text'
                 // {...register('email')}
-                id='email'
+                id='websiteURL'
+                name='websiteURL'
+                required
               />
             </div>
             <div className='form-item'>
-              <label htmlFor='email'>Cover Image *</label>
+              <label htmlFor='coverImage'>Cover Image *</label>
               <input
                 placeholder='...'
                 type='text'
                 // {...register('email')}
-                id='email'
+                id='coverImage'
+                name='coverImage'
+                required
               />
             </div>
             <div className='form-item'>
-              <label htmlFor='message'>Content *</label>
-              <textarea placeholder='Type your content...' id='message' />
+              <label htmlFor='markdown'>Content *</label>
+              <textarea placeholder='Type your content...' name='markdown' id='markdown' />
             </div>
           </div>
           <div className='form-item'>
@@ -81,10 +113,19 @@ const AddProject = (props: Props) => {
               <button className='btn btn-primary'>Add project</button>
             </div>
           </div>
-        </form>
+        </ValidatedForm>
       </div>
     </main>
   );
+};
+
+export const action = async ({ request }: ActionArgs) => {
+  const fieldValues = await validator.validate(await request.formData());
+  if (fieldValues.error) return validationError(fieldValues.error);
+  const project = fieldValues.data;
+
+  await addProject({ ...project, createdAt: new Date(), isFeatured: false });
+  return redirect('/admin/projects');
 };
 
 export default AddProject;
