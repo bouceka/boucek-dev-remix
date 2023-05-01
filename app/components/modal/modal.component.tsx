1b0d4b2a1753c069074d1706c30d9e0635a050b3
Adam Boucek <adamm01b@gmail.com>
2023-05-01 13:08:53 -0700

    feat: Add modal

---
+++ app/components/modal/modal.component.tsx
@@ -0,0 +1,56 @@
+// @flow
+import * as React from 'react';
+import { Action } from '../action/action.component';
+
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+  title: string;
+  desc?: string;
+  primaryAction: () => void;
+  secondaryAction?: () => void;
+  primaryBtnContent: string;
+  secondaryBtnContent?: string;
+};
+
+export const Modal = ({
+  open,
+  onClose,
+  title,
+  desc,
+  primaryAction,
+  secondaryAction,
+  primaryBtnContent,
+  secondaryBtnContent,
+}: Props) =>
+  !open ? null : (
+    <div onClick={onClose} className="modal__overlay">
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className="modal__container"
+      >
+        <div className="modal">
+          <button className="modal__close-btn" onClick={onClose}>
+            X
+          </button>
+          <div className="modal__content">
+            <h2 className="heading">{title}</h2>
+            <p className="paragraph--medium">{desc}</p>
+          </div>
+          <div className="modal__btn-container">
+            <Action as='button' styleType="primary" onClick={primaryAction}>
+              {primaryBtnContent}
+            </Action>
+            {secondaryAction ? (
+              <Action as='button' styleType="secondary" onClick={secondaryAction}>
+                {secondaryBtnContent}
+              </Action>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,34 @@
-import { type V2_MetaFunction } from '@remix-run/react';
+import { useNavigate, type V2_MetaFunction } from '@remix-run/react';
+import { useEffect, useState } from 'react';
 import { typedjson, useTypedLoaderData } from 'remix-typedjson';
 import Header from '~/components/header/header.component';
+import { Modal } from '~/components/modal/modal.component';
 import { ProjectList } from '~/components/project-list/project-list.component';
 import { getAllProjects } from '~/data/projects.server';
 
 export default function Index() {
   const { projects } = useTypedLoaderData<typeof loader>();
+  const [openModal, setOpenModal] = useState(false);
+  useEffect(() => {
+    const getWarning = localStorage.getItem('WIPModal');
+    if (!getWarning) {
+      setOpenModal(true);
+      localStorage.setItem('WIPModal', JSON.stringify('displayed'));
+    }
+  }, []);
   return (
     <>
       <Header />
+      <Modal
+        primaryBtnContent='Close'
+        primaryAction={() => {
+          setOpenModal(false);
+        }}
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        title='The portfolio is in WORK IN PROGRESS'
+        desc='This is a portfolio website of Adam Boucek. This app is using RemixJS and is still under construction.'
+      />
       <ProjectList projects={projects} />
     </>
   );
