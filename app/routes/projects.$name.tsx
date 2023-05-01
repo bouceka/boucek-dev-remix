11491800f0da00c29595f67209eccca9e5c7c724
Adam Boucek <adamm01b@gmail.com>
2023-05-01 13:09:58 -0700

    feat: Add error handler on project detail page

--- app/routes/projects.$name.tsx
+++ app/routes/projects.$name.tsx
@@ -1,16 +1,19 @@
 // @flow
 import type { LoaderArgs, V2_MetaFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { isRouteErrorResponse, useCatch, useLoaderData, useParams, useRouteError } from '@remix-run/react';
 import * as React from 'react';
 import invariant from 'tiny-invariant';
 import ReactMarkdown from 'react-markdown';
 import { getProject } from '~/data/projects.server';
+import { Action } from '~/components/action/action.component';
 
 export const loader = async ({ params }: LoaderArgs) => {
   invariant(params.name, `params.slug is required`);
   const project = await getProject(params.name);
-  invariant(project, `Project not found: ${params.slug}`);
+  if (!project) {
+    throw json({ message: 'Could not find any project' }, { status: 404, statusText: 'Not Found' });
+  }
 
   return json({ project });
 };
@@ -27,12 +30,22 @@ export const ProjectDetailPage = () => {
 
           <div className='project--references'>
             {project?.websiteURL && (
-              <a target='_blank' rel='noopener noreferrer' href={project?.websiteURL} className='project--references__link'>
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href={project?.websiteURL}
+                className='project--references__link'
+              >
                 Website
               </a>
             )}
             {project?.githubURL && (
-              <a target='_blank' rel='noopener noreferrer' href={project?.githubURL} className='project--references__link'>
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href={project?.githubURL}
+                className='project--references__link'
+              >
                 Github
               </a>
             )}
@@ -44,14 +57,40 @@ export const ProjectDetailPage = () => {
   );
 };
 
+export function ErrorBoundary() {
+  const error = useRouteError();
+  const params = useParams();
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return (
+      <main className='project-detail '>
+        <div className='row'>
+          <div className=''>Uh oh! The post with the slug "{params.name}" does not exist!</div>
+          <Action styleType='secondary' as='link' to='/#section-projects'>
+            Back to Projects
+          </Action>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Uh oh ...</h1>
+      <p>Something went wrong.</p>
+    </div>
+  );
+}
 export const meta: V2_MetaFunction = ({ data }) => {
+  if (!data) {
+    return [{ title: `Boucek Dev | Error` }, { name: 'description' }];
+  }
   return [
     {
-      title: `Boucek Dev | ${data.project.slug}`,
+      title: `Boucek Dev | ${data.project?.slug}`,
     },
     {
       name: 'description',
-      content: data.project.title,
+      content: data.project?.title,
     },
   ];
 };
