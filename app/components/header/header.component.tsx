6d85d00719ce31baefdb13f96e023ceb8ac37f1f
bouceka <adamm01b@gmail.com>
2023-05-05 13:14:24 -0700

    feat: Edit header arrow cta

--- app/components/header/header.component.tsx
+++ app/components/header/header.component.tsx
@@ -1,18 +1,18 @@
 // @flow
 import * as React from 'react';
 
- const Header: React.FC = () => {
+const Header: React.FC = () => {
   return (
-    <header className="header">
-      <div className="header__text-box">
-        {/* <span className="body-text-intro">Full stack developer</span> */}
-        <h1 className="heading heading__primary">Adam Boucek!</h1>
-        <span className="body-text-main ">Full Stack developer</span>
+    <header className='header'>
+      <div className='header__text-box'>
+        <h1 className='heading heading__primary'>Adam Boucek</h1>
+        <span className='p--xlarge'>Full Stack developer</span>
       </div>
-      <div className="header--arrow">
-        <a href="#section-projects">
-          <div className="arrow-button">
-            <span className="arrow"></span>
+      <div className='header--arrow'>
+        <a href='#section-projects' className='cta'>
+          <span className='btn btn--link'>See my projects</span>
+          <div className='arrow__button'>
+            <span className='arrow'></span>
           </div>
         </a>
       </div>
