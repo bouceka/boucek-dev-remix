be6e16d9916bd9cd1b3987b7b3ad560fbd755480
bouceka <adamm01b@gmail.com>
2023-06-17 12:22:02 -0700

    feat: Add create blog page in admin dash

--- app/routes/admin.blogs.tsx
+++ app/routes/admin.blogs.tsx
@@ -1,15 +1,34 @@
 // @flow
 import { LoaderFunction } from '@remix-run/node';
 import * as React from 'react';
+import { typedjson, useTypedLoaderData } from 'remix-typedjson/dist/remix';
+import { Action } from '~/components/action/action.component';
+import { AdminTable } from '~/components/admin-table/admin-table.component';
 import { requireUserSession } from '~/data/auth.server';
+import { getAllBlogPosts } from '~/data/blogs.server';
 type Props = {};
 const AdminBlogs = (props: Props) => {
-  return <div>AdminBlogs</div>;
+  const { blogPosts } = useTypedLoaderData<typeof loader>();
+  return (
+    <>
+      <div className='admin-heading'>
+        <h3>Admin Projects</h3>
+        <Action as='link' to='/admin/blog/add' styleType='primary'>
+          Add Blog Post
+        </Action>
+      </div>
+      <section>
+        <AdminTable data={blogPosts} />
+      </section>
+    </>
+  );
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
   await requireUserSession(request);
-  return null;
+  const blogPosts = await getAllBlogPosts();
+  return typedjson({ blogPosts });
 };
+
 
 export default AdminBlogs;
