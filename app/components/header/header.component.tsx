63b129f7fb32132e9fbf67e3420d59b89bc357a4
bouceka <adamm01b@gmail.com>
2023-06-24 22:17:43 -0700

    fix: Fix scroll feature

--- app/components/header/header.component.tsx
+++ app/components/header/header.component.tsx
@@ -1,12 +1,13 @@
 // @flow
 import * as React from 'react';
+import { animateScroll as scroll, Link, scroller, scrollSpy } from 'react-scroll';
 
 const Header: React.FC = () => {
   return (
     <header className='header'>
       <div className='header__text-box'>
         <h1 className='heading'>Adam Bouček</h1>
-          <span className='header__subtitle__main'>/ædəm baʊː tʃɛk/</span>
+        <span className='header__subtitle__main'>/ædəm baʊː tʃɛk/</span>
         <div className='header__bio'>
           <span className='header__subtitle'>noun</span>
           <span className='p--xxlarge'>Full stack developer, Graduated from NIC</span>
@@ -17,12 +18,21 @@ const Header: React.FC = () => {
         </div>
       </div>
       <div className='header--arrow'>
-        <a href='#projects' className='cta'>
-          <span className='btn btn--link'>See my projects</span>
+        <Link
+          href={'#!'}
+          to={'projects'}
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-50}
+          activeClass='active'
+          className={'cta'}
+        >
+          <span className='btn btn--link'>See my work</span>
           <div className='arrow__button'>
             <span className='arrow'></span>
           </div>
-        </a>
+        </Link>
       </div>
     </header>
   );
