52cb6b17eae529572d847d237b56df42128e9366
bouceka <adamm01b@gmail.com>
2023-06-24 22:24:29 -0700

    chore: Edit admin table

--- app/components/admin-table/admin-table.component.tsx
+++ app/components/admin-table/admin-table.component.tsx
@@ -1,40 +1,44 @@
 // @flow
-import type { Project } from '@prisma/client';
+import type { Blog, Project } from '@prisma/client';
 import * as React from 'react';
+import { Link } from '@remix-run/react';
 
 type Props = {
-  data: Project[];
+  data: Project[] | Blog[];
 };
-export const AdminTable = ({ data }: Props) => {
+export const AdminTable = ({ data, ...props }: Props) => {
   return (
-    <div className='admin-table'>
-      <table>
-        <thead>
-          <tr>
-            <th>Cover Image</th>
-            <th>Title</th>
-            <th>Slug</th>
-            <th>Created At</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={item.id}>
-              <td>
-                <img className='admin-table__image-preview' src={item.coverImage} alt={item.title} />
-              </td>
-              <td>{item.title}</td>
-              <td>{item.slug}</td>
-              <td>{item.createdAt.toDateString()}</td>
-              <td className='admin-table__actions'>
-                <button className='admin-table__btn-edit'>Edit</button>
-                <button className='admin-table__btn-delete'>Delete</button>
-              </td>
+    <>
+      <div className='admin-table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Cover Image</th>
+              <th>Title</th>
+              <th>Slug</th>
+              <th>Created At</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={item.id}>
+                <td>
+                  <img className='admin-table__image-preview' src={item.coverImage} alt={item.title} />
+                </td>
+                <td>{item.title}</td>
+                <td>{item.slug}</td>
+                <td>{item.createdAt.toDateString()}</td>
+                <td className='admin-table__actions'>
+                  <Link to={`/admin${(item as Project).githubURL ? `/project/${item.slug}` : `/blog/${item.slug}`}`} className='admin-table__btn-edit'>
+                    Edit
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
