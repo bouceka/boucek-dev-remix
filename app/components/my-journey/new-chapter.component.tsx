732376bb944147030072ff243049601a4a4f30d8
Adam Boucek <adam@boucek.dev>
2024-02-03 00:07:47 -0800

    Chore: Edit my journey text

--- app/components/my-journey/new-chapter.component.tsx
+++ app/components/my-journey/new-chapter.component.tsx
@@ -29,12 +29,11 @@ export const NewChapter = () => {
               introduce myself. I was so committed that I learned English at the academic level within a year.
             </p>
             <p>
-              I moved to Canada and started studying Advanced Digital Design and Development Diploma. It went so well
-              that I graduated a year earlier than I was supposed to. Al this could happen only because of all the
-              strong women around me and my inspiring grandmother who gave me crucial advice about my life in her very
-              last moment. Now I'm in my 3rd year of university, studying computer science. I'm passionate about making
-              web/mobile apps but seeing myself working in cybersecurity and working for a national organization to
-              fight cyber crimes.
+              I moved to Canada and studied for an Advanced Digital Design and Development Diploma. It went so well that
+              I graduated a year earlier than expected. All this could happen only because of all the strong women
+              around me and my inspiring grandmother, who gave me crucial advice about my life in her very last moment.
+              Now I'm in my 3rd year of university, studying computer science. I'm passionate about making web/mobile
+              apps, but I see myself working in cybersecurity and for a national organization to fight cyber crimes.
             </p>
           </div>
         </div>
